20328557868d08b18de6caef59b3da533a501dce
Mustafa Aytemür <maytemur@gmail.com>
2023-02-09 22:29:54 +0300

    Initial Commit

---
+++ 21ParseFotografPaylasma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  21ParseFotografPaylasma
+//
+//  Created by maytemur on 9.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
